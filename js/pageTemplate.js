ccfc25f257c774a3c5a34c9083d86ac326539d71
ryanharris <Ryan.Harris@onassignment.com>
2014-02-27 23:20:15 +0000

    Added ability to add templates and started correcting layout

--- js/pageTemplate.js
+++ js/pageTemplate.js
@@ -2,23 +2,28 @@ function TodoCtrl($scope,$http) {
   $http.get('/xml/PageTemplatePortfolio.xml',
     {transformResponse:function(data) {
       //console.log(data);
-                      // convert the data to JSON and provide
-                      // it to the success function below
-                        var x2js = new X2JS();
-                        var json = x2js.xml_str2json( data ).portfolio.pagetemplate;
-                        //console.log(json);
-                        return json;
-                        }
-                    }).
+      // convert the data to JSON and provide
+      // it to the success function below
+        var x2js = new X2JS();
+        var json = x2js.xml_str2json( data ).portfolio.pagetemplate;
+        //console.log(json);
+        return json;
+        }
+    }).
     success(function(data, status) {
       //console.log(data);
       $scope.portfolio = data;
   });
 
   $scope.addPageTemplate = function() {
+      console.log($scope.title);
     $scope.portfolio.push({
-      text:$scope.todoText, 
-      done:false
+      id:$scope.id, 
+      title:$scope.title,
+      description:$scope.description,
+      cost:$scope.cost,
+      smallpic:$scope.smallpic,
+      largepic:$scope.largepic
     });
 
     $scope.todoText = '';
