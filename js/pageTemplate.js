a05d3fa1a248e14c6c783dff0597bd1d6a2f7174
Ryan Harris <Ryan.Harris@onassignment.com>
2014-03-01 19:14:42 -0800

    Added carousel buttons and some style

--- js/pageTemplate.js
+++ js/pageTemplate.js
@@ -1,23 +1,28 @@
-function TodoCtrl($scope,$http) {
-  $http.get('/xml/PageTemplatePortfolio.xml',
-    {transformResponse:function(data) {
-      //console.log(data);
-      // convert the data to JSON and provide
-      // it to the success function below
-        var x2js = new X2JS();
-        var json = x2js.xml_str2json( data ).portfolio.pagetemplate;
-        //console.log(json);
-        return json;
-        }
-    }).
-    success(function(data, status) {
-      //console.log(data);
-      $scope.portfolio = data;
+var app=angular.module('myApp', []);
+
+function PageTemplateController($scope,$http) {
+  $http.get('/xml/PageTemplatePortfolio.xml', {
+    transformResponse:function(data) {
+      var x2js = new X2JS();
+      var json = x2js.xml_str2json( data ).portfolio.pagetemplate;
+      return json;
+    }
+  })
+  .success(function(data, status) {
+    $scope.portfolio = data;
+    $scope.id = data[0].id;
+    $scope.title = data[0].title;
+    $scope.description = data[0].description;
+    $scope.cost = data[0].cost;
+    $scope.smallpic = data[0].smallpic;
+    $scope.largepic = data[0].largepic;
+    $scope.currentPage = 0;
+    $scope.pageSize = 4;
   });
 
   $scope.addPageTemplate = function() {
       console.log($scope.title);
-    $scope.portfolio.push({
+      $scope.portfolio.push({
       id:$scope.id, 
       title:$scope.title,
       description:$scope.description,
@@ -25,32 +30,47 @@ function TodoCtrl($scope,$http) {
       smallpic:$scope.smallpic,
       largepic:$scope.largepic
     });
-
-    $scope.todoText = '';
   };
 
-  $scope.todos = [
-    {text:'learn angular', done:true},
-    {text:'build an angular app', done:false}];
-
-  $scope.addTodo = function() {
-    $scope.todos.push({text:$scope.todoText, done:false});
-    $scope.todoText = '';
-  };
- 
-  $scope.remaining = function() {
-    var count = 0;
-    angular.forEach($scope.todos, function(todo) {
-      count += todo.done ? 0 : 1;
+  $scope.showPageTemplate = function(id) {
+    angular.forEach($scope.portfolio, function(value, key){
+      if(value.id == id){
+        $scope.id = value.id;
+        $scope.title = value.title;
+        $scope.description = value.description;
+        $scope.cost = value.cost;
+        $scope.smallpic = value.smallpic;
+        $scope.largepic = value.largepic;
+      }
     });
-    return count;
   };
- 
-  $scope.archive = function() {
-    var oldTodos = $scope.todos;
-    $scope.todos = [];
-    angular.forEach(oldTodos, function(todo) {
-      if (!todo.done) $scope.todos.push(todo);
-    });
+
+  $scope.prevEnabled = function() {
+    if($scope.currentPage == 0) {
+      $scope.prevClass = 'page-button-disabled';
+      return true;
+    }
+    else {
+      $scope.prevClass = 'page-button';
+      return false;
+    }
+  };
+
+  $scope.nextEnabled = function() {
+    if($scope.currentPage >= $scope.portfolio.length/$scope.pageSize - 1) {
+      $scope.nextClass = 'page-button-disabled';
+      return true;
+    }
+    else {
+      $scope.nextClass = 'page-button';
+      return false;
+    }
   };
 }
+
+app.filter('startFrom', function() {
+    return function(input, start) {
+        start = +start;
+        return input.slice(start);
+    };
+});
